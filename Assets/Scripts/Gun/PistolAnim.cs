5e316743f3f54831b13a84988e47efc53a44dc2e
Ravine001 <szymon_gaming111@wp.pl>
2024-03-22 20:12:49 +0000

    pistol animations

--- Assets/Scripts/Gun/PistolAnim.cs
+++ Assets/Scripts/Gun/PistolAnim.cs
@@ -4,37 +4,18 @@ using UnityEngine;
 
 public class PistolAnim : MonoBehaviour
 {
-    public Animator anim;
+    
     public bool checkingMag;
     public float CheckMagRetDelay;
     public Transform pistolSlide;
     public Transform slideReturn;
     public float PistolSlideX;
     public float returnSpeed;
-    public Behaviour Recoil;
+    
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse1)){
-            anim.SetBool("IsAiming", true);
-            anim.SetBool("CheckingMag", false);
-        }
-        else if (!Input.GetKey(KeyCode.Mouse1)){
-            anim.SetBool("IsAiming", false);
-            anim.SetBool("CheckingMag", false);
-        }
-
-        if (Input.GetKey(KeyCode.T))
-        {
-            anim.SetBool("IsAiming", false);
-            anim.SetBool("CheckingMag", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.T))
-        {
-            
-            anim.SetBool("IsAiming", false);
-            anim.SetBool("CheckingMag", false);
-        }
+        
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
