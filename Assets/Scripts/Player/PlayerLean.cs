b8ae34a56a524b92fa14e61ad20add796e285635
Ravine001 <szymon_gaming111@wp.pl>
2024-03-22 20:55:48 +0000

    eeee

---
+++ Assets/Scripts/Player/PlayerLean.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLean : MonoBehaviour
+{
+    public float leanSpeed;
+    public KeyCode leanLeft;
+    public KeyCode leanRight;
+    public float leanReturnSpeed;
+
+
+    public Transform defaultLean;
+    public Transform leanObject;
+    public Transform m_Camera;
+    public Transform leanCalculationObject;
+
+    public float l_rotLeft;
+    public float l_rotRight;
+
+    private void Start()
+    {
+        leanObject.localRotation = Quaternion.Euler(0, 0, 0);
+        leanCalculationObject.localRotation = Quaternion.Euler(0, 0, 0);
+
+    }
+
+    public void Update()
+    {
+        leanCalculation();
+    }
+
+    private void leanCalculation()
+    {
+        if (Input.GetKey(leanLeft))
+        {
+            leanCalculationObject.localRotation = Quaternion.Euler(0, 0, l_rotLeft);
+            leanObject.localRotation = Quaternion.Lerp(leanObject.localRotation, leanCalculationObject.localRotation, leanSpeed * Time.deltaTime);
+        }
+        else
+        {
+            leanCalculationObject.localRotation = Quaternion.Euler(0, 0, 0);
+            leanObject.localRotation = Quaternion.Lerp(leanObject.localRotation, leanCalculationObject.localRotation, leanReturnSpeed * Time.deltaTime);
+        }
+    }
+}
